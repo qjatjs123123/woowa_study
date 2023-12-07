5520a0c7e6def4fd77470e6ce3e26cfd978d1767
Hong Boem Sun <qjatjs123123@naver.com>
2023-12-07 14:33:59 +0900

    feat: 방문 날짜 에러 발생하면 다시 입력받도록 구현

--- javascript-christmas-6/src/Controller/EventController.js
+++ javascript-christmas-6/src/Controller/EventController.js
@@ -2,6 +2,7 @@ import OutputView from '../View/OutputView.js';
 import Calendar from '../Domain/Calendar.js';
 import { OUTPUT_MESSAGE, INPUT_MESSAGE } from '../Util/Message.js';
 import InputView from '../View/InputView.js';
+import ErrorHandler from '../Util/ErrorHandler.js';
 
 class EventController {
   async start() {
@@ -10,8 +11,17 @@ class EventController {
   }
 
   async handleInputDate() {
-    const input = await InputView.read(INPUT_MESSAGE.date);
-    const calendar = new Calendar(input);
+    let calendar = '';
+    while (true) {
+      const input = await InputView.read(INPUT_MESSAGE.date);
+      const isValidate = ErrorHandler.handle(() => new Calendar(input));
+
+      if (isValidate) {
+        calendar = new Calendar(input);
+        break;
+      }
+    }
+    return calendar;
   }
 }
 
